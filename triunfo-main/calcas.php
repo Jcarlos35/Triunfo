6562f51c9a65fcda0af7ea1106e45b9d41081c3b
jc <jc203039@gmail.com>
2022-11-29 16:43:09 -0300

    calças nome

--- triunfo-main/calcas.php
+++ triunfo-main/calcas.php
@@ -18,7 +18,7 @@
                             <a href="comprar.php"><img src="imgs/calca1.jfif" alt="Placeholder image"></a>
                     </div>
                 </div class="has-background-grey-lighter">
-                <td><p class="has-text-centered has-text-grey"> Calça </p></td>
+                <td><p class="has-text-centered has-text-grey"> Calça Jeans Justa </p></td>
                 <td><p class="has-text-centered has-text-black"> <b>R$150,00</b></p></td>
                <div> <a class="has-text-black" href="comprar.php"><b>Comprar</b></a></div>
 
@@ -31,7 +31,7 @@
                             <a href="comprar.php"><img src="imgs/calca2.jfif" alt="Placeholder image"></a>
                     </div>
                 </div class="has-background-grey-lighter">
-                <td><p class="has-text-centered has-text-grey"> Calça </p></td>
+                <td><p class="has-text-centered has-text-grey"> Calça Jeans Justa Cinza  </p></td>
                 <td><p class="has-text-centered has-text-black"> <b>R$145,50</b></p></td>
                <div> <a class="has-text-black" href="comprar.php"><b>Comprar</b></a></div>
             </div>
@@ -44,7 +44,7 @@
                             <a href="comprar.php"><img src="imgs/calca3.jfif" alt="Placeholder image"></a>
                     </div>
                 </div class="has-background-grey-lighter">
-                <td><p class="has-text-centered has-text-grey"> Calça </p></td>
+                <td><p class="has-text-centered has-text-grey">  Calça Moletom </p></td>
                 <td><p class="has-text-centered has-text-black"> <b>R$160,00</b></p></td>
                <div> <a class="has-text-black" href="comprar.php"><b>Comprar</b></a></div>
 
@@ -56,7 +56,7 @@
                             <a href="comprar.php"><img src="imgs/calca4.jpg" alt="Placeholder image"></a>
                     </div>
                 </div class="has-background-grey-lighter">
-                <td><p class="has-text-centered has-text-grey"> Calça </p></td>
+                <td><p class="has-text-centered has-text-grey"> Calça Moletom </p></td>
                 <td><p class="has-text-centered has-text-black"> <b>R$156,99</b></p></td>
                <div> <a class="has-text-black" href="comprar.php"><b>Comprar</b></a></div>
             </div>
@@ -69,7 +69,7 @@
                             <a href="comprar.php"><img src="imgs/calca5.jfif" alt="Placeholder image"></a>
                         </figure>
                     </div class="has-background-grey-lighter">
-                    <td><p class="has-text-centered has-text-grey"> Calça </p></td>
+                    <td><p class="has-text-centered has-text-grey">  Calça Moletom </p></td>
                 <td><p class="has-text-centered has-text-black"> <b>R$154,50</b></p></td>
                <div> <a class="has-text-black" href="comprar.php"><b>Comprar</b></a></div>
                 </div>
@@ -82,7 +82,7 @@
                             <a href="comprar.php"><img src="imgs/calca6.jpg" alt="Placeholder image"></a>
                     </div>
                 </div class="has-background-grey-lighter">
-                <td><p class="has-text-centered has-text-grey"> Calça </p></td>
+                <td><p class="has-text-centered has-text-grey"> Calça Moletom </p></td>
                 <td><p class="has-text-centered has-text-black"> <b>R$167,99</b></p></td>
                <div> <a class="has-text-black" href="comprar.php"><b>Comprar</b></a></div>
             </div>
@@ -113,7 +113,7 @@
                             <a href="comprar.php"><img src="imgs/calcas7.jfif" alt="Placeholder image"></a>
                     </div>
                 </div class="has-background-grey-lighter">
-                <td><p class="has-text-centered has-text-grey"> Calça </p></td>
+                <td><p class="has-text-centered has-text-grey"> Calça Jeans Justa </p></td>
                 <td><p class="has-text-centered has-text-black"> <b>R$164,99</b></p></td>
                <div> <a class="has-text-black" href="comprar.php"><b>Comprar</b></a></div>
 
@@ -126,7 +126,7 @@
                             <a href="comprar.php"><img src="imgs/calcas8.jpg" alt="Placeholder image"></a>
                     </div>
                 </div class="has-background-grey-lighter">
-                <td><p class="has-text-centered has-text-grey"> Calça </p></td>
+                <td><p class="has-text-centered has-text-grey"> Calça Reta </p></td>
                 <td><p class="has-text-centered has-text-black"> <b>R$150,50</b></p></td>
                <div> <a class="has-text-black" href="comprar.php"><b>Comprar</b></a></div>
             </div>
@@ -139,7 +139,7 @@
                             <a href="comprar.php"><img src="imgs/calcas9.webp" alt="Placeholder image"></a>
                     </div>
                 </div class="has-background-grey-lighter">
-                <td><p class="has-text-centered has-text-grey"> Calça </p></td>
+                <td><p class="has-text-centered has-text-grey"> Calção Vermelho Vinho </p></td>
                 <td><p class="has-text-centered has-text-black"> <b>R$148,90</b></p></td>
                <div> <a class="has-text-black" href="comprar.php"><b>Comprar</b></a></div>
 
@@ -151,7 +151,7 @@
                             <a href="comprar.php"><img src="imgs/calcas10.jpg" alt="Placeholder image"></a>
                     </div>
                 </div class="has-background-grey-lighter">
-                <td><p class="has-text-centered has-text-grey"> Calça </p></td>
+                <td><p class="has-text-centered has-text-grey"> Calção </p></td>
                 <td><p class="has-text-centered has-text-black"> <b>R$154,90</b></p></td>
                <div> <a class="has-text-black" href="comprar.php"><b>Comprar</b></a></div>
             </div>
@@ -164,7 +164,7 @@
                             <a href="comprar.php"><img src="imgs/calcas11.webp" alt="Placeholder image"></a>
                         </figure>
                     </div class="has-background-grey-lighter">
-                    <td><p class="has-text-centered has-text-grey"> Calça </p></td>
+                    <td><p class="has-text-centered has-text-grey"> Calção </p></td>
                 <td><p class="has-text-centered has-text-black"> <b>R$159,99</b></p></td>
                <div> <a class="has-text-black" href="comprar.php"><b>Comprar</b></a></div>
                 </div>
@@ -178,7 +178,7 @@
                             <a href="comprar.php"><img src="imgs/calcas12.webp" alt="Placeholder image"></a>
                     </div>
                 </div class="has-background-grey-lighter">
-                <td><p class="has-text-centered has-text-grey"> Calça </p></td>
+                <td><p class="has-text-centered has-text-grey"> Calção </p></td>
                 <td><p class="has-text-centered has-text-black"> <b>R$164,00</b></p></td>
                <div> <a class="has-text-black" href="comprar.php"><b>Comprar</b></a></div>
             </div>
